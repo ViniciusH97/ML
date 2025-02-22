59c23c7c75c7c10db7823c87ac4d4d334f18a202
ViniciusH97 <vinicius.hl2010@gmail.com>
2025-02-22 13:53:55 -0300

    bibliotecas

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('teste')
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
