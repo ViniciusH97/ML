a8c7eb90b40d96f4e13dddce4136390f3ec6dba0
ViniciusH97 <vinicius.hl2010@gmail.com>
2025-02-22 13:59:11 -0300

    criando os dados

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
+
+# Criação de dados
+
+np.random.seed(0)
+x = 2 * np.random.rand(100, 1) # aqui ira receber números aleatórios de 1 a 100
+y = 4 + 3 * x + np.random.randn(100, 1)
+
